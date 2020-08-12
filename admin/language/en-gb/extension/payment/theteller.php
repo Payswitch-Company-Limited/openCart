1c4645770459dbe79b22523b04bbddef3dffc3e8
Marc Donald AHOURE <dmcorporation2014@gmail.com>
2020-08-12 14:04:10 +0000

    Updated plugin to 2.0
    Added sms feature , change Checkout endpoint

--- admin/language/en-gb/extension/payment/theteller.php
+++ admin/language/en-gb/extension/payment/theteller.php
@@ -6,14 +6,26 @@ $_['heading_title']		 = 'Theteller Payment Gateway';
 $_['text_extension']	 = 'Extensions';
 $_['text_success']		 = 'Success: You have modified Theteller account details!';
 $_['text_edit']          = 'Edit Theteller Payment Gateway';
-
+$_['text_theteller']	 = '<a href="https://theteller.net" target="_blank"><img src="view/image/payment/theteller.png" alt="Theteller" title="Theteller" style="border: 1px solid #EEEEEE;" /></a>';
 // Entry
 $_['entry_merchant_id']	 = 'Merchant ID';
-$_['entry_merchant_name'] = 'Merchant Name Or Shop Name';
+$_['entry_merchant_name'] = 'Merchant Name';
 $_['entry_api_user'] 	  = 'Api User';
 $_['entry_api_key'] 	  = 'Api Key';
 
 $_['entry_environment']	 = 'Environment';
+
+$_['entry_currency']	 = 'Currency';
+
+$_['entry_channel']		 = 'Channel';
+
+$_['entry_theteller_sms_status']		 = 'SMS Status';
+
+$_['entry_sms_api_user']		 = 'SMS Api User';
+
+$_['entry_sms_api_password']		 = 'SMS Api Password';
+
+$_['entry_sms_api_senderid']		 = 'SMS SenderID';
 
 
 $_['entry_callback']	 = 'Callback URL';
@@ -32,10 +44,21 @@ $_['help_merchant_name']     = 'Name displayed to user during checkout.';
 $_['help_api_user']		 = 'Api User  given by PaySwitch.';
 $_['help_api_key']		 = 'Api Key  given by PaySwitch.';
 $_['help_environment']	 = 'Change Environment to Test or Live for transaction on Theteller. Default is : Test';
+$_['help_currency']	 = 'Select your currency. Default is : GHS';
+$_['help_channel']	 = 'Select channel that you want to allow on the checkout page. Default is : Both ';
+$_['help_sms_status']	 = 'This feature allows you to send SMS to customer after successful purchase. Ensure that you have all your credentials details set.';
+$_['help_sms_api_user']	 = 'SMS API User given to Merchant by Theteller SMPP';
+$_['help_sms_api_password']	 = 'SMS API Password given to Merchant by Theteller SMPP';
+$_['help_sms_api_senderid']	 = 'Sender ID must be registred on Theteller SMPP. 11 characters Maximum';
 
 // Error
 $_['error_permission']	 = 'Warning: You do not have permission to modify payment Theteller!';
 $_['error_merchant_id']	 = 'Merchant ID Required!';
-$_['error_merchant_name']	 = 'Merchant Name or Shop Name Required!';
+$_['error_merchant_name']	 = 'Merchant Name Required !';
 $_['error_api_user']	 = 'Api User Required!';
 $_['error_api_key']	 	 = 'Api Key Required!';
+$_['error_currency']	 = 'Currency is Required!';
+$_['error_channel']	 	 = 'Channel is Required!';
+$_['error_sms_api_user'] = 'SMS Api user is Required!';
+$_['error_sms_api_password']	 = 'SMS Api password is Required!';
+$_['error_sms_senderid']	 	 = 'SMS SenderID is Required!';
